1b465c591fe65b2d379227f21c8add43396d9d44
ayham <[ayham10.aboajeb@gmail.com]>
2025-09-26 18:18:24 +0300

    fix bugs

--- src/controller/profile/handleUpdateAvatar.js
+++ src/controller/profile/handleUpdateAvatar.js
@@ -1,18 +1,20 @@
 const AppError = require("../../utils/AppError");
-const userModel=require('../../models/user');
-const handleUpdateAvatar = async(req, res, next) => {
+const userModel = require('../../models/user');
+const { uploadToVercelBlob } = require('../../utils/imageUploadVercelBlob')
+const handleUpdateAvatar = async (req, res, next) => {
     try {
         const userId = res.locals.id;
         if (!req.file) {
             throw new AppError(400, 'no specific image');
         }
-        const url = `/public/${req.file.filename}`;
-        const response=await userModel.findByIdAndUpdate(userId,{avatar:url});
-        if(response){
-            return res.status(200).send({msg:"avatar updated"});
+        const url = await uploadToVercelBlob(req.file);
+        // const url = `/public/${req.file.filename}`;
+        const response = await userModel.findByIdAndUpdate(userId, { avatar: url });
+        if (response) {
+            return res.status(200).send({ msg: "avatar updated" });
         }
     } catch (err) {
         next(err);
     }
 }
-module.exports=handleUpdateAvatar;
+module.exports = handleUpdateAvatar;
--- src/controller/profile/handleUpdateCoverImage.js
+++ src/controller/profile/handleUpdateCoverImage.js
@@ -1,18 +1,20 @@
 const userModel = require('../../models/user');
-const AppError = require('../../utils/AppError')
+const AppError = require('../../utils/AppError');
+const { uploadToVercelBlob } = require('../../utils/imageUploadVercelBlob');
 const handleUpdateCoverImage = async (req, res, next) => {
     try {
         const userId = res.locals.id;
         if (!req.file) {
             throw new AppError(400, 'no specific image');
         }
-        const url = `/public/${req.file.filename}`;
+        const url = await uploadToVercelBlob(req.file);
+        // const url = `/public/${req.file.filename}`;
         const response = await userModel.findByIdAndUpdate(userId, { coverImage: url });
-        if(response){
-            return res.status(200).send({msg:'cover image updated'});
+        if (response) {
+            return res.status(200).send({ msg: 'cover image updated' });
         }
     } catch (err) {
         next(err);
     }
 }
-module.exports=handleUpdateCoverImage
+module.exports = handleUpdateCoverImage
