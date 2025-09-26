a5d76932692b03e9478a0ee027913755af1a223b
ayham <[ayham10.aboajeb@gmail.com]>
2025-09-26 17:50:19 +0300

    add upload to vercel blob

--- src/controller/posts/handleCreatePost.js
+++ src/controller/posts/handleCreatePost.js
@@ -1,19 +1,21 @@
 const postModel = require('../../models/post');
+const { uploadToVercelBlob } = require('../../utils/imageUploadVercelBlob')
 const handleCreatePost = async (req, res, next) => {
     try {
         const userId = res.locals.id;
         let newPost = {};
-        if (req.files && req.files?.length > 0) {
-            const images = req.files?.map((e) => (`/public/${e.filename}`))
-            newPost.images = images;
+        if (req.files && req.files.length > 0) {
+            const uploadPromises = req.files?.map(file => uploadToVercelBlob(file));
+            const imageUrls = await Promise.all(uploadPromises);
+            newPost.images = imageUrls;
         }
         const postInformation = JSON.parse(req.body.postInformation);
         newPost = { ...newPost, ...postInformation, userId: userId };
-        console.log(newPost);
+
         const new_post = new postModel(newPost);
         const response = await new_post.save();
         if (response) {
-            return res.status(200).send({msg:"post created"});
+            return res.status(200).send({ msg: "post created" });
         }
     } catch (err) {
         next(err);
